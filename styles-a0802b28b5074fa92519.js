bbc0a29144bdc58c827c7682a1685c3de68f13eb
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2019-11-30 14:27:44 +0500

    Updates

---
+++ styles-a0802b28b5074fa92519.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{275:function(n,w,o){}}]);
+//# sourceMappingURL=styles-a0802b28b5074fa92519.js.map
--- sw.js
+++ sw.js
@@ -26,13 +26,13 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-13c6fb60f212c8f2d3bc.js"
+    "url": "webpack-runtime-1f358041498a9c9da339.js"
   },
   {
     "url": "commons.c6dceef504ade98e991f.css"
   },
   {
-    "url": "commons-0037bbd0c5ef8ead0178.js"
+    "url": "commons-84172f503844bdd7fab7.js"
   },
   {
     "url": "app-13a28d1403ec4481c4eb.js"
@@ -42,11 +42,11 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c826719e070de96f83c6ad6af017e930"
+    "revision": "b07a11537ab0fe91848abff4ab31908e"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "ae8f9fd1b4fe04953b10055dde474db0"
+    "revision": "d4111a94d3939b65da0ccc4e394d1b47"
   },
   {
     "url": "manifest.webmanifest",
